7f35a4bd8ad3d0cc158266eb28d9b9b929aec528
oceanwen <oceanwen>
2025-01-03 07:04:44 +0000

    init

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class HomeController extends Controller
 {
     public function index()
     {
-        var_dump(123123);exit;
-        return view('welcome');
+        var_dump($_REQUEST['laravel_session']);exit;
     }
 }

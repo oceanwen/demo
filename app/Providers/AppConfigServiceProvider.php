f3a93a3409492091a0ae7dfbe94262c2eb5790a4
oceanwen <oceanwen>
2025-01-12 12:09:15 +0000

    读取secretmanager

--- app/Providers/AppConfigServiceProvider.php
+++ app/Providers/AppConfigServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Aws\SecretsManager\SecretsManagerClient;
 use Aws\AppConfig\AppConfigClient;
 use Illuminate\Support\Facades\Config;
 
@@ -21,12 +22,28 @@ class AppConfigServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $client = new SecretsManagerClient([
+            'version' => 'latest',
+            'region' => 'ap-northeast-1',
+        ]);
+
+        $secretName = 'aksk';
+        $result = $client->getSecretValue([
+            'SecretId' => $secretName,
+        ]);
+
+        $secret = json_decode($result['SecretString'], true);
+        $awsAccessKeyId = $secret['AWS_ACCESS_KEY_ID'];
+        $awsSecretAccessKey = $secret['AWS_SECRET_ACCESS_KEY'];
+
+        dump($awsAccessKeyId,$awsSecretAccessKey);
+        
         $appConfig = new AppConfigClient([
             'version' => 'latest',
-            'region' => env('AWS_DEFAULT_REGION'),
+            'region' => 'ap-northeast-1',
             'credentials' => [
-                'key'    => env('AWS_ACCESS_KEY_ID'),
-                'secret' => env('AWS_SECRET_ACCESS_KEY'),
+                'key'    => $awsAccessKeyId,
+                'secret' => $awsSecretAccessKey,
             ]
         ]);
 
@@ -44,7 +61,7 @@ class AppConfigServiceProvider extends ServiceProvider
         $content = $contentStream->getContents();
         $configData = json_decode($content, true);
 
-        //dump($configData);
+        dump($configData);
         if (!empty($configData)) {
             Config::set('database.connections.mysql.host', $configData['host']);
             Config::set('database.connections.mysql.database', $configData['dbname']);
